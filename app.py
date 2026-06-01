7eb02173fd0988069ca639a12a70e1b7d3eacbf9
joaojo23 <johnshadrack0@yahoo.com>
2026-06-01 18:48:08 +0300

    Fix Railway error

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return jsonify({
+        "message": "Portfolio Backend Running"
+    })
 
 @app.route("/contact", methods=["POST"])
 def contact():
@@ -13,10 +15,10 @@ def contact():
     message = request.form.get("message")
 
     print(f"New message from {name} ({email}): {message}")
+
     return "Message received successfully!"
 
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
